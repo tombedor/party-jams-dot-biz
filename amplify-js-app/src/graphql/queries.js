2f66b2cda0bd2f85e41b85dc8275723446f3e9a3
Tom Bedor <tombedor@gmail.com>
2020-07-01 09:28:26 -0700

    add bucket

---
+++ amplify-js-app/src/graphql/queries.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getChant = /* GraphQL */ `
+  query GetChant($word: String!) {
+    getChant(word: $word) {
+      url
+      lyrics {
+        seconds
+        phrase
+      }
+    }
+  }
+`;
